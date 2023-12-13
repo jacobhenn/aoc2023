07ec0598717d2b480dbb0e56359c18f640fdb935
Jacob Henn <windowWeasel@outlook.com>
2023-12-13 15:05:25 -0800

    day 4 smoothisher

--- d4/src/lib.rs
+++ d4/src/lib.rs
@@ -28,6 +28,10 @@ fn score(card: &str) -> (usize, usize) {
 fn solve<Part: AocPart>(input: &str) -> usize {
     let mut cards: Vec<(&str, usize)> = input.lines().map(|line| (line, 1)).collect();
 
+    if Part::is_one() {
+        return cards.iter().map(|(card, _)| score(card).0).sum();
+    }
+
     for idx in 0..cards.len() {
         let (card, copies) = cards[idx];
         trace!("looking at Card {}", idx + 1);
@@ -44,22 +48,24 @@ fn solve<Part: AocPart>(input: &str) -> usize {
     cards.iter().map(|(card, count)| count).sum()
 }
 
-example_tests! {
-    - part one:
-        a0: "" => 0,
-    - part two:
-        b0: "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+const EXAMPLE_INPUT: &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11" => 30,
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+example_tests! {
+    - part one:
+        a0: EXAMPLE_INPUT => 13,
+    - part two:
+        b0: EXAMPLE_INPUT => 30,
 }
 
 #[test]
 fn part_one() {
     let _ = aocutil::test_logger().try_init();
-    assert_eq!(solve::<part::One>(&aocutil::get_input(YEAR, DAY)), 0);
+    assert_eq!(solve::<part::One>(&aocutil::get_input(YEAR, DAY)), 21959);
 }
 
 #[test]
