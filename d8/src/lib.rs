4292486870eb6e9e86a5eee11b2e4a4c811e8186
Jacob Henn <windowWeasel@outlook.com>
2023-12-19 11:27:38 -0800

    day 8 part 2

--- d8/src/lib.rs
+++ d8/src/lib.rs
@@ -1,19 +1,18 @@
 use aocutil::prelude::*;
 
-const YEAR: usize = 2023;
+pub const YEAR: usize = 2023;
 
-const DAY: usize = 8;
+pub const DAY: usize = 8;
 
-fn solve_a(instrs: &str, nodes: HashMap<&str, (&str, &str)>) -> usize {
-    let mut node = "AAA";
-    for (idx, instr) in instrs.chars().cycle().enumerate() {
-        match instr {
-            'L' => node = nodes.get(node).expect("node should be present").0,
-            'R' => node = nodes.get(node).expect("node should be present").1,
-            other => panic!("invalid R/L instruction {other}"),
+fn solve_a(steps: &[Step], nodes: HashMap<Node, (Node, Node)>) -> usize {
+    let mut node = Node::aaa();
+    for (idx, step) in steps.iter().cycle().enumerate() {
+        match step {
+            Step::Left => node = nodes.get(&node).expect("node should be present").0,
+            Step::Right => node = nodes.get(&node).expect("node should be present").1,
         }
 
-        if node == "ZZZ" {
+        if node.is_zzz() {
             return idx + 1;
         }
     }
@@ -21,43 +20,270 @@ fn solve_a(instrs: &str, nodes: HashMap<&str, (&str, &str)>) -> usize {
     unreachable!()
 }
 
-fn solve_b(instrs: &str, nodes: HashMap<&str, (&str, &str)>) -> usize {
-    todo!();
+#[derive(Copy, Clone, Hash, Debug, PartialEq, Eq)]
+enum Step {
+    Left,
+    Right,
+}
 
-    let mut current_nodes: HashSet<&str> = nodes
-        .keys()
-        .filter_map(|n| n.ends_with('A').then_some(*n))
-        .collect();
+impl Step {
+    fn from_char(c: char) -> Self {
+        match c {
+            'L' => Self::Left,
+            'R' => Self::Right,
+            other => panic!("step should be L or R, found {other}"),
+        }
+    }
+}
 
-    for (idx, instr) in instrs.chars().cycle().enumerate() {
-        current_nodes = current_nodes
-            .into_iter()
-            .map(|node| match instr {
-                'L' => nodes.get(node).expect("node should be present").0,
-                'R' => nodes.get(node).expect("node should be present").1,
-                other => panic!("invalid R/L instruction {other}"),
-            })
-            .collect();
+#[derive(Copy, Clone, Hash, Debug, PartialEq, Eq)]
+struct Node {
+    name: [char; 3],
+}
 
-        if current_nodes.iter().all(|n| n.ends_with('Z')) {
-            return idx + 1;
+impl Display for Node {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for c in self.name {
+            f.write_char(c)?;
+        }
+        Ok(())
+    }
+}
+
+impl FromStr for Node {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self {
+            name: s
+                .chars()
+                .fill_array::<3>()
+                .context("node names should be three letters")?,
+        })
+    }
+}
+
+impl Node {
+    fn aaa() -> Self {
+        Self {
+            name: ['A', 'A', 'A'],
         }
     }
 
-    todo!()
+    fn is_zzz(&self) -> bool {
+        self.name == ['Z', 'Z', 'Z']
+    }
+
+    fn is_z(&self) -> bool {
+        self.name[2] == 'Z'
+    }
+
+    fn is_a(&self) -> bool {
+        self.name[2] == 'A'
+    }
 }
 
-fn solve<Part: AocPart>(input: &str) -> usize {
+/// All the information you need to store any infinite sequence of nodes, as they must repeat.
+#[derive(Clone)]
+struct Orbit {
+    /// The first element of a pair represents the step that the corresponding node was visited on.
+    nodes: Vec<(usize, Node)>,
+
+    /// The index of the first node of the cyclic part. If you extended `nodes` to include one more,
+    /// it would the same as this one.
+    cycle_start: usize,
+
+    /// The offset you must add to the steps in the cyclic part to get to the next cycle. If you
+    /// extended `nodes` to include one more, this would be `nodes.last().0 - nodes[cycle_start].0`
+    cycle_step: usize,
+}
+
+impl Display for Orbit {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for (t, node) in &self.nodes[..self.cycle_start] {
+            write!(f, "(t={t} {node}) ")?;
+        }
+
+        write!(f, "r[")?;
+
+        for (t, node) in &self.nodes[self.cycle_start..] {
+            write!(f, "(t={t} {node}) ")?;
+        }
+
+        write!(f, "]")?;
+
+        write!(f, " step: {}", self.cycle_step)?;
+
+        Ok(())
+    }
+}
+
+impl Orbit {
+    fn cycle_length(&self) -> usize {
+        self.nodes.len() - self.cycle_start
+    }
+
+    fn into_iter(&self) -> impl Iterator<Item = (usize, Node)> + '_ {
+        self.nodes[0..self.cycle_start].iter().copied().chain(
+            (0..)
+                .map(move |cycle| {
+                    self.nodes[self.cycle_start..]
+                        .iter()
+                        .copied()
+                        .map(move |(step, node)| (step + cycle * self.cycle_step, node))
+                })
+                .flatten(),
+        )
+    }
+
+    /// Fast-forward to the nth entry in the sequence (not the value at the nth global step).
+    fn nth(&self, n: usize) -> (usize, Node) {
+        if n < self.cycle_step {
+            self.nodes[n]
+        } else {
+            let (t, node) =
+                self.nodes[self.cycle_start + (n - self.cycle_start) % self.cycle_length()];
+
+            (
+                t + self.cycle_step * ((n - self.cycle_start) / self.cycle_length()),
+                node,
+            )
+        }
+    }
+
+    /// Takes as input:
+    /// - the sequence of global L/R steps
+    /// - an iterator of pairs of un-modded step numbers and nodes
+    #[instrument(level = "trace", skip_all)]
+    fn from_iter<'a>(steps: &[Step], nodes_iter: impl Iterator<Item = (usize, Node)>) -> Self {
+        // TMP: this is just for debugging purposes
+        let mut prev_max_t = 0;
+
+        // keys are pairs of modded step numbers and nodes. values are indices of `nodes` that
+        // these occurred on.
+        let mut history: HashMap<(usize, Node), usize> = HashMap::new();
+
+        // just a collection of items from `nodes` - step numbers remain unmodded.
+        let mut nodes: Vec<(usize, Node)> = Vec::new();
+
+        for (t, node) in nodes_iter {
+            trace!("got (t={t} {node})");
+
+            if t < prev_max_t {
+                panic!("t should be strictly increasing");
+            } else {
+                prev_max_t = t;
+            }
+
+            if let Some(cycle_start) = history.insert((t % steps.len(), node), nodes.len()) {
+                trace!("found cycle; start={cycle_start}");
+
+                return Self {
+                    cycle_step: t - nodes[cycle_start].0,
+                    nodes,
+                    cycle_start,
+                };
+            }
+
+            nodes.push((t, node));
+        }
+
+        unreachable!("sequence should be infinite")
+    }
+}
+
+fn iterate_sequence<'a>(
+    steps: &'a [Step],
+    nodes: &'a HashMap<Node, (Node, Node)>,
+    mut node: Node,
+) -> impl Iterator<Item = (usize, Node)> + 'a {
+    steps.iter().cycle().enumerate().map(move |(t, step)| {
+        let res = (t, node);
+
+        let &(l, r) = nodes.get(&node).expect("node should be in map");
+        match step {
+            Step::Left => node = l,
+            Step::Right => node = r,
+        }
+
+        res
+    })
+}
+
+fn solve_b(steps: &[Step], nodes: &HashMap<Node, (Node, Node)>) -> usize {
+    let mut a_node_orbits: HashMap<Node, Orbit> = HashMap::new();
+
+    // STEP 1: find the orbits of all A nodes so that finding their nth iterates is trivial
+
+    for &a_node in nodes.keys().filter(|node| node.is_a()) {
+        let orbit = Orbit::from_iter(steps, iterate_sequence(steps, nodes, a_node));
+
+        debug!("node {a_node}: {orbit}");
+
+        a_node_orbits.insert(a_node, orbit);
+    }
+
+    // STEP 2: look at the orbits of successive A nodes under the sequences of Z nodes of the
+    // previous node.
+
+    let mut a_nodes = nodes.keys().filter(|node| node.is_a());
+
+    let first_a_node = *a_nodes.next().expect("there is at least one A node");
+    trace!("starting with {first_a_node}");
+
+    let mut current_orbit = a_node_orbits[&first_a_node].clone();
+
+    for a_node in a_nodes {
+        trace!("looking at {a_node}");
+        // trace!("current orbit: {current_orbit}");
+
+        let a_node_orbit = &a_node_orbits[a_node];
+
+        trace!(
+            "current_orbit cycle_start: {}, cycle_step: {}",
+            current_orbit.cycle_start,
+            current_orbit.cycle_step
+        );
+
+        current_orbit = Orbit::from_iter(
+            steps,
+            current_orbit
+                .into_iter()
+                .filter(|(t, node)| {
+                    if node.is_z() {
+                        trace!("found z node in current orbit: t={t} {node}");
+                    }
+                    node.is_z()
+                })
+                .map(|(t, _)| {
+                    let (u, res) = a_node_orbit.nth(t);
+                    trace!("{t}th {a_node} iterate: t={u} {res}");
+                    (u, res)
+                }),
+        );
+    }
+
+    // STEP 3: find the first Z node in the remaining orbit
+
+    let (first_z_step, _) = current_orbit
+        .into_iter()
+        .find(|(_, node)| node.is_z())
+        .expect("orbits should be infinite");
+
+    first_z_step
+}
+
+pub fn solve<Part: AocPart>(input: &str) -> usize {
     let mut lines = input.lines();
 
-    let instrs = lines.next().unwrap();
-    lines.next(); // blank line
+    let steps: Vec<Step> = lines.next().unwrap().chars().map(Step::from_char).collect();
 
-    let nodes: HashMap<&str, (&str, &str)> = lines
+    let nodes: HashMap<Node, (Node, Node)> = lines
+        .skip(1)
         .map(|line| {
             unlist!(
                 line.split(|c: char| !c.is_ascii_alphanumeric())
-                    .filter(|s| !s.is_empty()),
+                    .filter_map(|s| s.parse::<Node>().ok()),
                 name,
                 l,
                 r
@@ -67,9 +293,9 @@ fn solve<Part: AocPart>(input: &str) -> usize {
         .collect();
 
     if Part::is_one() {
-        solve_a(instrs, nodes)
+        solve_a(&steps, nodes)
     } else {
-        solve_b(instrs, nodes)
+        solve_b(&steps, &nodes)
     }
 }
 
@@ -111,5 +337,8 @@ fn part_one() {
 #[test]
 fn part_two() {
     let _ = aocutil::test_logger().try_init();
-    assert_eq!(solve::<part::Two>(&aocutil::get_input(YEAR, DAY)), 0);
+    assert_eq!(
+        solve::<part::Two>(&aocutil::get_input(YEAR, DAY)),
+        22289513667691
+    );
 }
