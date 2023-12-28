9c65ca00602b9d5ce42e4b5bb540c0464b782a66
Jacob Henn <windowWeasel@outlook.com>
2023-12-28 15:43:34 -0800

    day 18 part 2

--- d18/src/lib.rs
+++ d18/src/lib.rs
@@ -18,11 +18,11 @@ impl Step {
 
         let direction = match P::part() {
             Part::One => RookDirection::from_udlr_positive_down(dir).expect("direction is valid"),
-            Part::Two => match color.chars().last().expect("color is non-empty") {
-                '0' => RookDirection::PLUS_X,
-                '1' => RookDirection::PLUS_Y,
-                '2' => RookDirection::MINUS_X,
-                '3' => RookDirection::MINUS_Y,
+            Part::Two => match &color[5..] {
+                "0" => RookDirection::PLUS_X,
+                "1" => RookDirection::PLUS_Y,
+                "2" => RookDirection::MINUS_X,
+                "3" => RookDirection::MINUS_Y,
                 other => panic!("invalid direction suffix '{other}'"),
             },
         };
@@ -36,70 +36,68 @@ impl Step {
     }
 }
 
-fn trench_slices(
-    steps: impl IntoIterator<Item = Step>,
-) -> HashMap<i64, BTreeMap<i64, HashSet<RookDirection>>> {
-    let mut slices: HashMap<i64, BTreeMap<i64, HashSet<RookDirection>>> = HashMap::new();
+fn corner_ys(steps: &[Step]) -> BTreeSet<i64> {
+    let mut corner_ys = BTreeSet::new();
 
     let mut pos = v!(0, 0);
 
     for step in steps {
-        for i in 0..=step.length {
-            if i != 0 {
-                pos += step.direction;
-            }
+        pos += step.direction.unit_vector::<i64>() * step.length;
 
-            slices
-                .entry(pos[1])
-                .or_default()
-                .entry(pos[0])
-                .or_default()
-                .insert(step.direction);
-        }
+        corner_ys.insert(pos[1]);
     }
 
-    slices
+    corner_ys
 }
 
-#[instrument(level = "trace", ret)]
-fn slice_area(slice: &BTreeMap<i64, HashSet<RookDirection>>) -> i64 {
+/// The pairs are (x, y)
+fn trench_slice(steps: &[Step], slice_y: i64) -> BTreeMap<i64, (bool, LineDirection)> {
+    let mut slice = BTreeMap::new();
+
+    let mut pos = v!(0, 0);
+
+    for step in steps {
+        let dst = pos + (step.direction.unit_vector::<i64>() * step.length);
+
+        if step.direction.is_vertical() {
+            if Range::ordered(pos[1], dst[1]).contains(&slice_y) {
+                slice
+                    .entry(pos[0])
+                    .or_insert((false, step.direction.sign()))
+                    .1 = step.direction.sign();
+            }
+        } else {
+            if pos[1] == slice_y {
+                slice
+                    .entry(pos[0])
+                    .or_insert((true, LineDirection::Negative))
+                    .0 = true;
+
+                slice
+                    .entry(dst[0])
+                    .or_insert((true, LineDirection::Negative))
+                    .0 = true;
+            }
+        }
+
+        pos = dst;
+    }
+
+    slice
+}
+
+fn slice_area(slice: &BTreeMap<i64, (bool, LineDirection)>) -> i64 {
     let mut area = 0;
 
     let mut entered_lagoon: Option<i64> = None;
 
     let mut entered_trench: Option<(i64, LineDirection)> = None;
 
-    for (&x, directions) in slice {
-        if directions.len() == 1 {
-            // straight horizontal or vertical
-            if directions
-                .iter()
-                .next()
-                .expect("should be exactly one direction")
-                .is_vertical()
-            {
-                // vertical tile; flip inside-outside
-                if let Some(entered_x) = entered_lagoon {
-                    area += x - entered_x + 1;
-                    entered_lagoon = None;
-                } else {
-                    entered_lagoon = Some(x);
-                }
-            } else {
-                // horizontal tile; do nothing
-            }
-        } else {
-            // we're at a bend
-
-            let vertical_direction = directions
-                .iter()
-                .filter(|d| d.is_vertical())
-                .next()
-                .expect("should be two directions, one vertical");
-
+    for (&x, &(is_corner, vertical_direction)) in slice {
+        if is_corner {
             if let Some((entered_trench_x, entered_trench_direction)) = entered_trench {
                 // we are at the end of a stretch
-                if entered_trench_direction != vertical_direction.sign() {
+                if entered_trench_direction != vertical_direction {
                     // in-and-back stretch
                     if entered_lagoon.is_none() {
                         // we were outside the lagoon; just add this stretch
@@ -119,8 +117,14 @@ fn slice_area(slice: &BTreeMap<i64, HashSet<RookDirection>>) -> i64 {
                 entered_trench = None;
             } else {
                 // we are at the beginning of a stretch
-                assert_eq!(entered_trench, None);
-                entered_trench = Some((x, vertical_direction.sign()));
+                entered_trench = Some((x, vertical_direction));
+            }
+        } else {
+            if let Some(entered_x) = entered_lagoon {
+                area += x - entered_x + 1;
+                entered_lagoon = None;
+            } else {
+                entered_lagoon = Some(x);
             }
         }
     }
@@ -129,23 +133,23 @@ fn slice_area(slice: &BTreeMap<i64, HashSet<RookDirection>>) -> i64 {
 }
 
 pub fn solve<Part: AocPart>(input: &str) -> i64 {
-    if Part::is_two() {
-        todo!("part 2 currently might cause an out-of-memory crash");
+    let steps: Vec<Step> = input.lines().map(Step::from_str::<Part>).collect();
+
+    let corner_ys = corner_ys(&steps);
+
+    let mut area = 0;
+
+    for (&corner_y, &next_corner_y) in corner_ys.iter().tuple_windows() {
+        area += slice_area(&trench_slice(&steps, corner_y));
+
+        let next_gap_len = next_corner_y - corner_y - 1;
+        area += next_gap_len * slice_area(&trench_slice(&steps, corner_y + 1));
     }
 
-    let steps = input.lines().map(Step::from_str::<Part>);
+    let last_corner_y = corner_ys.iter().last().expect("at least one corner y");
+    area += slice_area(&trench_slice(&steps, *last_corner_y));
 
-    let slices = trench_slices(steps);
-
-    trace!("{:#?}", slices);
-
-    slices
-        .iter()
-        .map(|(i, s)| {
-            trace!("slice {i}");
-            slice_area(s)
-        })
-        .sum()
+    area
 }
 
 aoc_tests! {
@@ -187,6 +191,6 @@ L 2 .",
 
     part::Two {
         eb0: e0 => 952408144115,
-        rb: @input => 0,
+        rb: @input => 250022188522074,
     }
 }
